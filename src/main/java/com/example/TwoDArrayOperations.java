1a6106942d7ece68145914c530d04a33ef7cb027
Kaoru-kubota-Karura <cosmo.kaoru1211@gmail.com>
2023-12-04 20:58:14 +0800

    I change the laptop.

--- src/main/java/com/example/TwoDArrayOperations.java
+++ src/main/java/com/example/TwoDArrayOperations.java
@@ -5,35 +5,77 @@ public class TwoDArrayOperations {
     public static void main(String[] args) {
         // Exercise 1: Initialize and Print a 2D Array
         // TODO: Initialize a 2D array of integers and print its contents in a matrix form.
+        int[][] array = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        printArray(array);
     }
 
     // Exercise 2: Find the Maximum Value in a 2D Array
     public static int findMaximumValue(int[][] array) {
-        // TODO: Implement the logic to find the maximum value in the 2D array.
-        return 0; // Placeholder return value
+        int max = Integer.MIN_VALUE;
+        for (int[] row : array) {
+            for (int value : row) {
+                max = Math.max(max, value);
+            }
+        }
+        return max;
     }
 
     // Exercise 3: Check for a Specific Value in a 2D Array
     public static boolean containsValue(int[][] array, int value) {
-        // TODO: Implement the logic to check if the 2D array contains the specified value.
-        return false; // Placeholder return value
+        for (int[] row : array) {
+            for (int element : row) {
+                if (element == value) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     // Exercise 4: Calculate the Average of All Elements in a 2D Array
     public static double calculateAverage(int[][] array) {
-        // TODO: Implement the logic to calculate the average of all elements in the 2D array.
-        return 0.0; // Placeholder return value
+        int sum = 0;
+        int count = 0;
+        for (int[] row : array) {
+            for (int value : row) {
+                sum += value;
+                count++;
+            }
+        }
+        return (double) sum / count;
     }
 
     // Exercise 5: Sum of Diagonal Elements in a 2D Array
     public static int sumDiagonal(int[][] array) {
-        // TODO: Calculate and return the sum of diagonal elements in the 2D array.
-        return 0; // Placeholder return value
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i][i];
+        }
+        return sum;
     }
 
     // Exercise 6: Flatten a 2D Array into 1D Array
     public static int[] flattenArray(int[][] array) {
-        // TODO: Implement the logic to flatten the 2D array into a 1D array.
-        return new int[0]; // Placeholder return value
+        int rows = array.length;
+        int cols = array[0].length;
+        int[] result = new int[rows * cols];
+        int index = 0;
+        for (int[] row : array) {
+            for (int value : row) {
+                result[index++] = value;
+            }
+        }
+        return result;
+    }
+    
+
+    // 確認用
+    private static void printArray(int[][] array) {
+        for (int[] row : array) {
+            for (int value : row) {
+                System.out.print(value + " ");
+            }
+            System.out.println();
+        }
     }
 }
